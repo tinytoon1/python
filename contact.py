b05ab2d22dd3053a89708756835a47a4f98391c4
tinytoon1 <nani2020@yandex.ru>
2016-07-12 22:27:50 +0300

    Добавлен класс Contact

---
+++ contact.py
@@ -0,0 +1,8 @@
+
+class Contact:
+    def __init__(self, firstname, lastname, company, address, mobile):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.company = company
+        self.address = address
+        self.mobile = mobile
