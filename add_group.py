6b2b4e4d87462b4087382365988578f84e8b0022
tinytoon1 <nani2020@yandex.ru>
2016-07-17 12:50:51 +0300

    Переименована функция создания фикстуры

--- add_group.py
+++ add_group.py
@@ -6,7 +6,7 @@ from application import Application
 
 
 @pytest.fixture
-def app_fixture(request):
+def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
