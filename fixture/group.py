d4bae114699e14d9e8f0c70d4ff53b1aab5eb412
tinytoon1 <nani2020@yandex.ru>
2016-07-17 18:09:59 +0300

    Создана общая фикстура

--- fixture/group.py
+++ fixture/group.py
@@ -30,6 +30,15 @@ class GroupHelper:
         # return to groups page
         self.return_to_groups_page()
 
+    def delete_first(self):
+        wd = self.app.wd
+        # open groups page
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
