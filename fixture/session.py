71d9f7915f883441b92d19892c00b71af9e55347
tinytoon1 <nani2020@yandex.ru>
2016-07-17 14:29:55 +0300

    Выделен класс для управления сессиями

---
+++ fixture/session.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+class SessionHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        # open homepage
+        self.app.open_homepage()
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
