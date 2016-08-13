fbd3adfb6a2d709f651fd90081e1df132568fcc6
tinytoon1 <nani2020@yandex.ru>
2016-08-13 18:13:49 +0300

    Реализована загрузка тестовых данных из файлов json
    Для тестов добавления групп и контактов

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@
 import pytest
 import json
 import os.path
+import jsonpickle
 from fixture.application import Application
 
 fixture = None
@@ -35,3 +36,15 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("json_"):
+            test_data = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, test_data, ids=[str(x) for x in test_data])
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as data:
+        return jsonpickle.decode(data.read())
--- tests/add_group.py
+++ tests/add_group.py
@@ -1,22 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-import random
-import string
 
 
-def random_data(prefix, max_len):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join(random.choice(symbols) for i in range(random.randrange(max_len)))
-
-
-test_data = [Group(name="", header="", footer="")] + \
-            [Group(name=random_data("name", 10), header=random_data("header", 20), footer=random_data("footer", 20))
-                for i in range(5)]
-
-
-@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
-def test_add_group(app, group):
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_groups()
     app.group.add(group)
     new_groups = app.group.get_groups()
